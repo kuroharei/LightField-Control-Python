769c03e94979a7dce3e8875b329fa29c326f99ae
Kuroha Rei <kuroharei.xyz@gmail.com>
2023-07-20 16:52:33 +0800

    Methods of capture data.

---
+++ sources/capture_data.py
@@ -0,0 +1,57 @@
+# Import the .NET class library
+import clr
+
+# Import python sys module
+import sys
+
+# Import os module
+import os
+
+# Import System.IO for saving and opening files
+from System.IO import *
+
+# Import c compatible List and String
+from System import String
+from System.Collections.Generic import List
+
+# Add needed dll references
+sys.path.append(os.environ['LIGHTFIELD_ROOT'])
+sys.path.append(os.environ['LIGHTFIELD_ROOT']+"\\AddInViews")
+clr.AddReference('PrincetonInstruments.LightFieldViewV5')
+clr.AddReference('PrincetonInstruments.LightField.AutomationV5')
+clr.AddReference('PrincetonInstruments.LightFieldAddInSupportServices')
+
+# PI imports
+from PrincetonInstruments.LightField.Automation import Automation
+from PrincetonInstruments.LightField.AddIns import *
+from PrincetonInstruments.LightField.AddIns import *
+
+
+def device_found():
+    # Find connected device
+    for device in experiment.ExperimentDevices:
+        if device.Type == DeviceType.Camera:
+            return True
+     
+    # If connected device is not a camera inform the user
+    print("Camera not found. Please add a camera and try again.")
+    return False  
+
+# Create the LightField Application (true for visible)
+# The 2nd parameter forces LF to load with no experiment 
+auto = Automation(True, List[String]())
+
+application = auto.LightFieldApplication
+experiment = application.Experiment
+filemanager = application.FileManager
+
+experiment.Load("FSRS-Blaze")
+
+# Check for device and inform user if one is needed
+if device_found()==True:
+
+    file_name = "SRS_data.spe"
+
+    data = experiment.Capture(1000)
+
+    filemanager.SaveFile(data, file_name)
